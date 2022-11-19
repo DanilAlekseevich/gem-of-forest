7e14f66358701ba455288d35f61adebbcd43f0c7
DanilAlekseevich <trofimov.danil.alekseevich@gmail.com>
2022-11-19 16:59:41 +0400

    refactor

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(xn----7sbbrccbq3afn6cs9c.online www.xn----7sbbrccbq3afn6cs9c.online)
 rsa_key_size=4096
-data_path="./.certbot"
+data_path="./data/certbot"
 email="trofimov.danil.alekseevich@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
