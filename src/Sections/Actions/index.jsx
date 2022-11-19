2fc8cafd465ebad57dfd5ec56c60ad02474d095b
DanilAlekseevich <trofimov.danil.alekseevich@gmail.com>
2022-11-19 14:18:07 +0400

    Change images

--- src/Sections/Actions/index.jsx
+++ src/Sections/Actions/index.jsx
@@ -4,18 +4,17 @@ import { Title } from '../../components/Title';
 import { ActionCard } from '../../components/ActionCard';
 import styles from './Actions.module.scss';
 
+import bath from '../../assets/img/actions/bath.png';
+import billiards from '../../assets/img/actions/billiards.png';
+import gazebos from '../../assets/img/actions/gazebos.png';
+import fishing from '../../assets/img/actions/fishing.png';
+
 export function Actions() {
   const actions = [
-    ['Баня на дровах', 'https://mdolina.ru//userfls/site/1920/824_malskaya-russkaya-banya.jpg'],
-    ['Бильярд', 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Russian_billiards_balls.jpg'],
-    [
-      'Беседки',
-      'https://pkgreenwood.ru/wa-data/public/shop/products/14/06/614/images/3672/dacha_x.970.jpg',
-    ],
-    [
-      'Рыбалка на пруду',
-      'https://www.peredelka.tv/upload/iblock/0fd/8fe308da6f7bb0a60cb47143ffbb8c1c.jpg',
-    ],
+    ['Баня на дровах', bath],
+    ['Бильярд', billiards],
+    ['Беседки', gazebos],
+    ['Рыбалка на пруду', fishing],
   ];
 
   return (
