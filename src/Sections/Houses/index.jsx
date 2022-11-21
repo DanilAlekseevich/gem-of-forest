42b6f22e1ef0e0e29479c671f2e31593b6cba847
Dmitrii <dmitriialekseenkoog@gmail.com>
2022-11-21 12:37:55 +0400

    adaptive carousel withoit 640

--- src/Sections/Houses/index.jsx
+++ src/Sections/Houses/index.jsx
@@ -55,26 +55,94 @@ export function Houses() {
         hourly: '800',
       },
     },
+    {
+      title: 'Дом 4 большой',
+      imgs: [
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+      ],
+      capacity: '20-25',
+      descr:
+        'Два этажа. На первом — кухня и баня, на втором — гостиная и три спальни. Три кровати + два дивана.',
+      cost: {
+        weekday: '7000',
+        dayof: '8000',
+        hourly: '800',
+      },
+    },
+    {
+      title: 'Дом 5 большой',
+      imgs: [
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+      ],
+      capacity: '20-25',
+      descr:
+        'Два этажа. На первом — кухня и баня, на втором — гостиная и три спальни. Три кровати + два дивана.',
+      cost: {
+        weekday: '7000',
+        dayof: '8000',
+        hourly: '800',
+      },
+    },
+    {
+      title: 'Дом 6 большой',
+      imgs: [
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+        'https://gde.ru/images/img_ru/474x354/4f/bf/4fbf5dd8be584c0a2124b9534ac923ce.jpg',
+      ],
+      capacity: '20-25',
+      descr:
+        'Два этажа. На первом — кухня и баня, на втором — гостиная и три спальни. Три кровати + два дивана.',
+      cost: {
+        weekday: '7000',
+        dayof: '8000',
+        hourly: '800',
+      },
+    },
   ];
 
   const settings = {
     slidesToShow: 2,
     slidesToScroll: 2,
-    // centerMode: true,
-    // variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 330,
+        settings: {
+          // rows: 1,
+          // slidesPerRow: 1000,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          // centerMode: true,
+          // variableWidth: true,
+        },
+      },
+    ],
   };
 
   return (
     <section className={styles.houses} id="houses">
       <div className="container">
         <Title text="Дома" />
-        <div className={styles.housesItems}>
-          <Slider {...settings}>
-            {houses.map((house) => (
-              <HouseCard {...house} />
-            ))}
-          </Slider>
-        </div>
+        {/* <div className={styles.housesItems}> */}
+        <Slider {...settings}>
+          {houses.map((house) => (
+            <HouseCard {...house} />
+          ))}
+        </Slider>
+        {/* </div> */}
       </div>
     </section>
   );
--- src/Sections/Review/index.jsx
+++ src/Sections/Review/index.jsx
@@ -48,20 +48,31 @@ export function Review() {
   const settings = {
     slidesToShow: 3,
     slidesToScroll: 3,
-    // variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: true,
+          variableWidth: true,
+          rows: 1,
+        },
+      },
+    ],
   };
 
   return (
     <section className={styles.review}>
       <div className="container">
         <Title text="Отзывы" />
-        <div className={styles.reviewWrapper}>
-          <Slider {...settings}>
-            {reviews.map((review, i) => (
-              <ReviewCard key={i} {...review} />
-            ))}
-          </Slider>
-        </div>
+        {/* <div className={styles.reviewWrapper}> */}
+        <Slider {...settings}>
+          {reviews.map((review, i) => (
+            <ReviewCard key={i} {...review} />
+          ))}
+        </Slider>
+        {/* </div> */}
       </div>
     </section>
   );
